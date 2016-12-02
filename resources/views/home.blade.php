68022837d8da486f62df00e03494a924c4672f05
LaurensAl <Clipsedesign@gmail.com>
2016-12-02 14:46:33 +0100

    composer update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
+@extends('master')
+ @section('title', 'Home')
+
+ @section('content')
+    <div class="container">
+         <div class="content">
+             <div class="title">Home Page</div>
+             <div class="quote">Our Home page!</div>
+             </div>
+         </div>
+     @endsection
+
 <html>
  <head>
      <title>Home Page</title>
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css\
- /bootstrap.min.css">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css\
- /bootstrap-theme.min.css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
 
       <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\
- "></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 
      </head>
  <body>
-<div class="container">
-    <div class="content">
-         <div class="title">Home Page</div>
-         <div class="quote">Our Home page!</div>
-         </div>
-     </div>
+
  </body>
   </html>
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,20 +1,16 @@
 <html>
-2 <head>
-    3 <title> @yield('title') </title>
-    4 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6\
-5 /css/bootstrap.min.css">
-    6 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6\
-7 /css/bootstrap-theme.min.css">
-    8
-    9 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-    10 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.mi\
-11 n.js"></script>
-    12 </head>
-13 <body>
-14
-15 @include('shared.navbar')
-16
-17 @yield('content')
-18
-19 </body>
-20 </html>
+ <head>
+     <title> @yield('title') </title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
+
+     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+     </head>
+ <body>
+
+ @include('navbar')
+
+ @yield('content')
+
+ </body>
