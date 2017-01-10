fdbbc88e5644353c1bff6fa81dc59480bd10973e
LaurensAl <Clipsedesign@gmail.com>
2017-01-10 10:38:45 +0100

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,7 +17,7 @@
 
                 <h2 style="font-family: 'Pacifico', cursive;" class=" row col-md-12">{!! $post->title !!}
                 </h2>
-                <div class="row">
+                {{--<div class="row">--}}
                     <div class="col-md-4">
                         <div><br>
                             <img src="/images/{!! $post->foto !!}"
@@ -26,7 +26,7 @@
                     </div>
                     <div>
                         <div class="col-md-8">
-                            <h3 style="font-family: 'Pacifico', cursive;">How to prepare:</h3>
+                            <h3 style="font-family: 'Pacifico', cursive;">Description:</h3>
                             {!! mb_substr($post->description,0,500) !!}
                             <h6><a href="viewblog/{{$post->id}}">Read more...</a></h6>
                         </div>
@@ -38,7 +38,7 @@
 
 
                     @endif
-                </div>
+                {{--</div>--}}
     </div>
     <div class="col-md-12 text-right">{!! $posts->render() !!}</div>
 
